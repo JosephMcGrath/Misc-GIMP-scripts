efffbe5a5a96546d1110a654ab63ee6d86a24113
Joe McGrath <joseph.mcgrath@outlook.com>
2019-01-26 22:53:13 +0000

    Added functionality to the file list & flood warning snippets

--- python_3/small_functions/list_files.py
+++ python_3/small_functions/list_files.py
@@ -1,21 +1,24 @@
 # -*- coding: utf-8 -*-
 
 import os
+from typing import List
 
-def list_files(dir_in : str) -> str:
+
+def list_files(dir_in: str) -> str:
     """Recursively lists all the files in a directory."""
-    return [os.path.join(dp, f)
-            for dp, _, filenames in os.walk(dir_in)
-            for f in filenames
-            ]
+    return [
+        os.path.join(dp, f) for dp, _, filenames in os.walk(dir_in) for f in filenames
+    ]
 
-def filter_extensions(file_list : list, extensions : list) -> list:
+
+def filter_extensions(file_list: List[str], extensions: List[str]) -> List[str]:
     """Filters a list of files based on their extensions."""
     if type(extensions) == str:
         extensions = [extensions]
     extensions = set([x.lower() for x in extensions])
-    
-    return [x
-            for x in file_list
-            if os.path.splitext(x)[1].lower() in extensions
-            ]
+    return [x for x in file_list if os.path.splitext(x)[1].lower() in extensions]
+
+
+def filtered_file_list(dir_in: str, extensions: List[str]) -> List[str]:
+    """Returns files in the directory with matching extensions."""
+    return filter_extensions(list_files(dir_in), extensions)
--- python_3/web_scrapers/ea_flood_warning/ea_flood_warning.py
+++ python_3/web_scrapers/ea_flood_warning/ea_flood_warning.py
@@ -6,29 +6,28 @@ import zipfile
 import os
 
 class fetch_ea_rg():
-    
+
     base_url = 'http://environment.data.gov.uk/flood-monitoring/archive/readings-{}.csv'
-    
+
     def __init__(self, year, month, day, dir_out = ''):
         self.date = datetime.date(year, month, day)
-        
+
         self.dir_out = dir_out
         self.base_raw = r'readings-{}.csv'
         self.base_file = os.path.join(dir_out, r'readings-{}.csv')
         self.base_zip = os.path.join(dir_out, r'readings-{}.zip')
-    
+
     def gen_url(self):
         return self.base_url.format(self.date.isoformat())
     
     def gen_file(self):
         return self.base_raw.format(self.date.isoformat())
-    
+
     def gen_file_name(self):
         return self.base_file.format(self.date.isoformat())
-    
+
     def gen_zip_name(self):
         return self.base_zip.format(self.date.strftime('%Y-%m'))
-    
     def fetch_data(self):
         """Download the data as a csv."""
         if not self.is_in_dir():
@@ -37,17 +36,17 @@ class fetch_ea_rg():
                                     )
             with open(self.gen_file_name(), 'wb') as f:
                 f.write(self.data.content)
-    
+
     def fetch_to_zip(self):
         """Fetch a day's worth of data and move it direct to the archive."""
         if not self.is_in_zip():
             self.fetch_data()
             self.move_to_zip()
-    
+
     def add_days(self, ndays = 1):
         """Add a number of days to the target date."""
         self.date += datetime.timedelta(days = ndays)
-    
+
     def move_to_zip(self, remove_original = True):
         """Move a file from the download path to the zip archive."""
         if not self.is_in_zip():
@@ -58,17 +57,17 @@ class fetch_ea_rg():
                         )
             if remove_original:
                 os.remove(self.gen_file_name())
-    
+
     def is_in_dir(self):
         return os.path.isfile(self.gen_file_name())
-    
+
     def is_in_zip(self):
         with zipfile.ZipFile(self.gen_zip_name(), mode='r') as zf:
             return self.gen_file() in zf.namelist()
-    
+
     def is_fetched(self):
         return self.is_in_dir() or self.is_in_zip()
-    
+
     def fetch_seq(self, days):
         """Fetch a sequence of days."""
 
